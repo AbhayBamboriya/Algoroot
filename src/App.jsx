9a3b3d90ed38d50480fbf31aae80f5e6e88d38cc
AbhayBamboriya <bamboriya09@gmail.com>
2025-03-28 18:56:30 +0530

    changes made

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,15 @@ import { Routes, Route } from 'react-router-dom';
 
 import './App.css'
 import PrivateRoute from './components/PrivateRoute';
-import HomePage from './pages/homePage';
+// import HomePage from './pages/homePage';
+import HomePage from './pages/HomePage';
 import DetailsPage from './pages/DetailPage';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<HomePage/>} />
         <Route 
           path="/details" 
           element={
